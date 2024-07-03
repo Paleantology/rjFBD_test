d62657410227d09d0b940d5ea6149c5b20986fb9
Elijah Phifer <ephifer21@gmail.com>
2024-07-03 12:33:01 -0500

    adding data

---
+++ scripts/rj_run.sh
@@ -0,0 +1,66 @@
+root="~/rjFBD_test"
+base="${root}/data/Latest_sim_data"
+data="${base}/data"
+scripts="${base}/scripts"
+
+nTips=(10 10 10 10 25 25 25 25 50 50 50 50)
+Mu=(1 2 3 4 1 2 3 4 1 2 3 4)
+trees=$(seq 1 100)
+psi=(1 2)
+mats=$(seq 1 100)
+
+#make a folder for a common place to accsess files
+common="${base}/common_place"
+
+#set with combo to work in
+n=10
+mu=1
+  
+my_dir="${base}/MU_${n}_${mu}"
+
+echo "Working in ${my_dir}"
+
+cd ${root}
+
+for tree in $trees; do
+    tree_dir="${my_dir}/${tree}"
+    mat_dir="${tree_dir}/mat"
+    mat1="${mat_dir}/1"
+    mat1="${mat_dir}/2"
+    #file="${tree_dir}/tree_${tree}.nex.csv"
+    
+    foss1="${mat_dir}/tree1_${tree}.nex.csv"
+    foss2="${mat_dir}/tree2_${tree}.nex.csv"
+    
+    cp ${foss1} ${common}/data_file.csv
+
+    find "$mat1" -name '*.nex' | while read matrix
+        do
+        #cd ${mat_dir}
+        cp $matrix ${common}/matrix.nex
+        #cd "/Users/elijahphifer/Documents/lab_work"
+        rb ${scripts}/rjFBD.Rev
+        #mkdir -p output/${mu}_${n}/${tree}/
+        cp output/low.log ${base}/output/${matrix}_low.log
+        cp output/low.trees ${base}/output/${matrix}_low.trees
+        #cp ${base}/output/samp_anc.mcc.tre ${base}/output/$matrix.mcc.tre
+        
+    done
+    
+    
+    cp ${foss2} ${common}/data_file.csv
+
+    find "$mat2" -name '*.nex' | while read matrix
+        do
+        #cd ${mat_dir}
+        cp $matrix ${common}/matrix.nex
+        #cd "/Users/elijahphifer/Documents/lab_work"
+        rb ${scripts}/rjFBD.Rev
+        #mkdir -p output/${mu}_${n}/${tree}/
+        cp output/low.log ${base}/output/${matrix}_low.log
+        cp output/low.trees ${base}/output/${matrix}_low.trees
+        #cp ${base}/output/samp_anc.mcc.tre ${base}/output/$matrix.mcc.tre
+        
+    done
+    
+done
